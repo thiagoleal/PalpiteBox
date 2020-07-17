a4d36c4b1047b0b69bf0287a6556fc13709fc5c6
Thiago Leal <thfleale@gmail.com>
2020-07-17 17:22:29 -0300

    Fix header width

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -3,7 +3,7 @@ import Header from '../Header';
 import Footer from '../Footer';
 
 const Layout = ({ children }) => (
-  <div className="flex flex-col h-screen">
+  <div className="flex flex-col w-screen h-screen">
     <Header />
     <div id="container" className="container mx-auto flex-1">
       {children}
